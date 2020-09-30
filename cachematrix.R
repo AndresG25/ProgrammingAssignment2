a5f2b5d0dcdd0561cc4d2e0c03d8f4945a6edd4b
Andres <andres25@utp.edu.co>
2020-09-29 21:39:07 -0500

    Method for solving the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,62 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# Creates a special array object that can be cached in its reverse 
+makeCacheMatrix <- function(h = matrix()) {
+  #Initialize the reverse property
+  j <- NULL
+  
+  #Form to configure the matrix
+  set <- function(y) {
+    h <<- y
+    j <<- NULL
+  }
+  #Way for obtaining the matrix
+  get <- function() {
+  h
+  }
+  #Way to set the inverse of the matrix
+  setinverse <- function(inverse){
+    j <<- inverse
+  }
+  
+  #Way to obtain the inverse of the matrix
+  getinverse <- function(){
+    j
+  }
+  #Returns a list of the methods
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+  
+  cacheSolve <- function(h, ...) {
+    # Return a matrix that is the inverse of 'x'
+    j <- h$getinverse()
+    # Only returns the reverse if already configured
+    if (!is.null(j)) {
+      message("getting cached data")
+      return(j)
+    }
+    #Get the Matrix
+    d <- h$get()
+    # Calculate the inverse 
+    j <- solve(d, ...)
+    # Set the inverse to the object
+    h$setinverse(j)
+    #Returns the inverse and print the value
+    j
+  }
+  
+  ## For testing the model, you can try with the following code: 
+  # B <- matrix(c(1,2,3,4),2,2)
+  # B1 <- makeCacheMatrix(B)
+  # cacheSolve(B1)
+
+
